817a33a21b616924532034c69ca4bdd5116aa6ab
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-09-07 03:44:26 +0000

    printed numbers

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,1 +1,10 @@
 print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(0%10)
+print(10/15)
+# The last number should keep repeating but instead it stops
