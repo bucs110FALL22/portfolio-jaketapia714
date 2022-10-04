c55c74b12133129c30f29ef20b004993e2efc7dd
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-10-04 02:15:55 +0000

    coinflip exercise completed

--- ch01/exercises/touch.py
+++ ch01/exercises/touch.py
@@ -8,3 +8,8 @@ print(10%15)
 print(0%10)
 print(10/15)
 # The last number should keep repeating but instead it stops
+
+rate=input("What is the current exchange rate for the Euro to Dollar?")
+
+amount=input("What amount of currency would you like to exchange?")
+
---
+++ ch05/exercises/coinflip.py
@@ -0,0 +1,21 @@
+import turtle
+import random
+jake=turtle.Turtle()
+jake.color("blue")
+jake.shape("turtle")
+jake.setpos(0,0)
+window=turtle.Screen()
+turtle.screensize(width=1000,height=1000)
+width=turtle.window_width()
+height=turtle.window_height()
+
+while abs(jake.xcor())<(width/2) and abs(jake.ycor())<(height/2):
+  result=random.randrange(2)
+  if result ==1:
+    jake.left(90)
+    jake.forward(50)
+    print("heads")
+  else:
+    jake.right(90)
+    jake.forward(50)
+    print("tails")
