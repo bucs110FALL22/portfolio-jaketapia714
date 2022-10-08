650186a577b4ac815c5af880e4690afdbf41a463
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-10-08 04:00:29 +0000

    start

---
+++ ch04/lab/main.py
@@ -0,0 +1,12 @@
+import pygame
+
+import random
+
+import math
+
+# partA
+pygame.init()
+window = pygame.display.set_mode([300,300])
+window.fill("red")
+pygame.draw.circle(window,"blue", (,),())
+
--- ch05/exercises/coinflip.py
+++ ch05/exercises/coinflip.py
@@ -6,8 +6,8 @@ jake.shape("turtle")
 jake.setpos(0,0)
 window=turtle.Screen()
 turtle.screensize(width=1000,height=1000)
-width=turtle.window_width()
-height=turtle.window_height()
+width= turtle.window_width()
+height= turtle.window_height()
 
 while abs(jake.xcor())<(width/2) and abs(jake.ycor())<(height/2):
   result=random.randrange(2)
