62ae366a27895913d61797e4a72c4e09e84d264c
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-10-12 20:54:24 +0000

    Finished exercise

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,6 +1,9 @@
 def star_pyramid():
   "This is a function that prints a pyramid of asterisks with a certain number of rows based on user input"
 rows= int(input("How many rows would like?"))
-for i in range(rows+1):
+for i in range(1,rows+1):
   print("*"*i,"\n")
   star_pyramid()
+for i in range(1,rows+i):
+ print("*"*(rows-i),"\n")
+ star_pyramid()
