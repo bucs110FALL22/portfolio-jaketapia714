6c762f6a76bbc25994b95a6f3ccc271573e35014
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-10-25 01:12:33 +0000

    GUI Classes

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,16 @@
+class Tube:
+  def __init__(self):
+    self.color="green"
+    self.height= 25
+    self.mario_on_top= True
+
+class Goomba:
+  def __init__(self):
+    self.moving_forward= True
+    self.lives=1
+    self.hp=1
+class Player:
+  def __init__(self):
+    self.player_num=1
+    self.lives=3
+    self.is_small=True
