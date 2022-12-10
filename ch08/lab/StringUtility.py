3059ec0b3b99776794bfa1f9016e3f6708dd4bea
Jake Tapia <112728067+jaketapia714@users.noreply.github.com>
2022-12-10 07:09:20 +0000

    Progress on ch08 lab

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -24,3 +24,43 @@ def bothEnds(self):
         return f"{self.string[0]}{self.string[1]}{self.string[length-2]}{self.string[length-1]}"
       else:
         return ""
+
+  def fixStart(self):
+    if len(self.string) <=1:
+      return self.string
+    first_letter = self.string[0]
+    string = self.string
+    for i in self.string:
+      if i == first_letter:
+        string = string.replace(i,"*")
+    string = string.replace("*",first_letter,1)
+    return string
+
+  def asciiSum(self):
+    string = self.string
+    sum = 0
+    for i in string:
+      sum = sum + ord(i)
+    return(sum)
+
+  def cipher(self):
+    length = len(self.string)
+    my_list = ''
+    for i in range(length):
+      if ord(self.string[i]) in range(65, 91):
+        if ord(self.s[i]) + length <= 90:
+          my_list = my_list + chr(ord(self.string[i]) + length)
+        else:
+          my_list = my_list + chr(ord("A") + chr(ord(self.string[i]) + length - 90)
+                                  
+      elif ord(self.string[i]) in range(97, 123):
+                                  
+        if ord(self.string[i]) + length <= 122:
+          my_list = my_list + chr(ord(self.string[i]) + length)
+        else:
+          my_list = my_list + chr(ord("A") + chr(ord(self.string[i]) + length - 90)
+
+      else:
+        my_list = my_list + self.string[i]
+    return my_list
+
